a50f29cdfa6454888edbc3ec13a20b4f05787f54
Leonardo Fuentes <leonardo.fuentes@gmail.com>
2022-04-28 23:20:38 -0400

    add missing annotation

--- wikidataApi/src/main/java/cl/mti/wikidataApi/validation/WikiDataApiAdvice.java
+++ wikidataApi/src/main/java/cl/mti/wikidataApi/validation/WikiDataApiAdvice.java
@@ -4,10 +4,12 @@ import cl.mti.wikidataApi.model.Error;
 import cl.mti.wikidataApi.model.ErrorCodes;
 import cl.mti.wikidataApi.model.ErrorResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ControllerAdvice
 public class WikiDataApiAdvice {
 
     @ExceptionHandler(EntityNotFoundException.class)
