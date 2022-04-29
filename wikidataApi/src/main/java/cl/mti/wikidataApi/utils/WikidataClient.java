3d6f18f3ca485e9856fc824ab61f8a8685451373
Leonardo Fuentes <leonardo.fuentes@gmail.com>
2022-04-29 00:32:04 -0400

    update error handler

--- wikidataApi/src/main/java/cl/mti/wikidataApi/utils/WikidataClient.java
+++ wikidataApi/src/main/java/cl/mti/wikidataApi/utils/WikidataClient.java
@@ -3,7 +3,6 @@ package cl.mti.wikidataApi.utils;
 import cl.mti.wikidataApi.model.WikiDataEntity;
 import cl.mti.wikidataApi.model.WikiDataResponse;
 import cl.mti.wikidataApi.validation.WikidataEntityNotFoundException;
-import cl.mti.wikidataApi.validation.WikidataServerException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,14 +14,12 @@ public class WikidataClient {
 
         RestTemplate restTemplate = new RestTemplate();
         String url = "https://www.wikidata.org/entity/"+entityStr;
-        ResponseEntity<WikiDataResponse> response = restTemplate.getForEntity(url, WikiDataResponse.class);
-
-        System.out.println("Status: "+response.getStatusCode());
-
-        if (response.getStatusCode().value() >= 400 && response.getStatusCode().value() <= 499)
+        ResponseEntity<WikiDataResponse> response;
+        try {
+            response = restTemplate.getForEntity(url, WikiDataResponse.class);
+        }catch (Exception e){
             throw new WikidataEntityNotFoundException(entityStr);
-        else if (response.getStatusCode().value() >= 500 && response.getStatusCode().value() <= 599)
-            throw new WikidataServerException();
+        }
 
         return Optional.ofNullable(response.getBody().getEntities().get(entityStr));
     }
