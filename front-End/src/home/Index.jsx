1a480467f39b4f3db68e813f55354a0340b5d76c
lmontanares123 <102386759+lmontanares123@users.noreply.github.com>
2022-05-26 21:55:49 -0400

    home

---
+++ front-End/src/home/Index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Home() {
+    return (
+        <div>
+            <h1>MTI21-Sistemas y Aplicaciones Web</h1>
+            
+        </div>
+    );
+}
+
+export { Home };
