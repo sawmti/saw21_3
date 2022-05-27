3b4e08ae47bde0125fe2a24d7d83bfc08ae41265
mariocolihueque <104262678+mariocolihueque@users.noreply.github.com>
2022-05-26 21:32:09 -0400

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' },
+                    { loader: 'less-loader' }
+                ]
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+              }
+        ]
+    },
+    resolve: {
+        mainFiles: ['index', 'Index'],
+        extensions: ['.js', '.jsx'],
+        alias: {
+            '@': path.resolve(__dirname, 'src/'),
+        }
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html'
+    })],
+    devServer: {
+        historyApiFallback: true
+    },
+    externals: {
+        // global app config object
+        config: JSON.stringify({
+            apiUrl: '/api'
+        })
+    }
+}
